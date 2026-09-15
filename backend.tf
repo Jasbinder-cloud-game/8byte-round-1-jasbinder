683763c5051743ebb7e47e30d3cc9e01dee1d018
Jasbinder singh <jasbindersingh@Jasbinders-MacBook-Air.local>
2026-09-16 01:51:55 +0530

    ran tf fmt

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-backend-jumbo"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket       = "tf-state-backend-jumbo"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
