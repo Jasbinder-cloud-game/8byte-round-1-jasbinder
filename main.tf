e4e2823bba4e3b739942ecc360625ad32c29e417
Jasbinder singh <jasbindersingh@Jasbinders-MacBook-Air.local>
2026-09-16 17:14:53 +0530

    adding route table subnet association for public subnets

--- main.tf
+++ main.tf
@@ -70,3 +70,15 @@ resource "aws_route_table" "public_subnet_to_internet" {
     Name = "public-subnet-route-table"
   }
 }
+
+#route table association for public subnets
+
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_subnet_to_internet.id
+}
+
+resource "aws_route_table_association" "public_subnet_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_subnet_to_internet.id
+}
